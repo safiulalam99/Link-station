d57dad80e3c6e9b53e3753abc6e9da8fa8d86450
Safiul Alam <82179767+safiulalam99@users.noreply.github.com>
2022-11-04 17:45:50 +0200

    refactors

--- station.js
+++ station.js
@@ -15,6 +15,7 @@ function get_linkStation_points(linkStationTest) {
   var coordinates = [];
   for (var i = 0; i < linkStationTest.length; i++) {
     coordinates.push([linkStationTest[i][0], linkStationTest[i][1]]);
+    
   }
   return coordinates;
 }
@@ -28,23 +29,23 @@ function get_reach_points(linkStationTest) {
 }
 
 // function to calculate distance between point and link station (values sent as parameter in form of [x,y])
-function device_distance(testValue, arr) {
-  var distance = Math.sqrt(
-    Math.pow(testValue[0] - arr[0], 2) + Math.pow(testValue[1] - arr[1], 2)
+function device_distance(testValue, points) {
+  return Math.sqrt(
+    Math.pow(testValue[0] - points[0], 2) +
+      Math.pow(testValue[1] - points[1], 2)
   );
-  return distance;
 }
 
 // function for calculating all distances between points and links (one by one)
-function total_distance(testValue, arr) {
+function total_distance(testValue, points) {
   var smallestDistance = [];
   for (var i = 0; i < testValue.length; i++) {
     // loop for point
     var smallestLimit = [999999];
-    for (var j = 0; j < arr.length; j++) {
+    for (var j = 0; j < points.length; j++) {
       // loop for link station
-      if (device_distance(testValue[i], arr[j]) < smallestLimit[0]) {
-        smallestLimit = [device_distance(testValue[i], arr[j]), j]; // getting smallestLimit value
+      if (device_distance(testValue[i], points[j]) < smallestLimit[0]) {
+        smallestLimit = [device_distance(testValue[i], points[j]), j]; // getting smallestLimit value
       }
     }
     smallestDistance.push(smallestLimit); // pushing smallest value for point i
@@ -52,50 +53,38 @@ function total_distance(testValue, arr) {
   return smallestDistance;
 }
 
-// function to calculate power takes linkStation from top of code ie starting values and smallestDistances from function Total_distance
-// function power(linkStation, smallestDistances) {
-//   var reach = [];
-//   for (var i = 0; i < linkStation.length; i++) {
-//     reach.push(linkStation[i][2]); // pushing last value(r) from linkStation arr (x,y,r)
-//   }
-//   var powerValues = (smallestDistance, reach) => {
-//     // inline function to calculate power for given point with smallestDistance and index of linkStation which had that smallest distance
-//     return smallestDistance.map((m) => {
-//       let distance = m[0];
-//       let stationIndex = m[1];
-//       if (distance > reach[stationIndex]) {
-//         console.log("No link station"); // condition as given in prompt
-//         return 0;
-//       }
-//       var power = Math.round(Math.pow(reach[stationIndex] - distance, 2)); // calculating power as per math function
-//       return power;
-//     });
-//   };
-//   return powerValues(smallestDistances, reach);
-// }
 
 
-function power(linkStation, smallestDistances) {
-  // var reach = [];
-  // for (var i = 0; i < linkStation.length; i++) {
-  //   reach.push(linkStation[i][2]); // pushing last value(r) from linkStation arr (x,y,r)
-  // }
+function get_power(linkStation, smallestDistances) {
   var powerValues = (smallestDistance, reach) => {
     // inline function to calculate power for given point with smallestDistance and index of linkStation which had that smallest distance
     return smallestDistance.map((m) => {
-      let distance = m[0];
-      let stationIndex = m[1];
-      if (distance > reach[stationIndex]) {
+      if (m[0] > reach[m[1]]) {
         console.log("No link station"); // condition as given in prompt
         return 0;
       }
-      var power = Math.round(Math.pow(reach[stationIndex] - distance, 2)); // calculating power as per math function
+      var power = Math.pow(reach[m[1]] - m[0], 2).toFixed(2); // calculating power as per math function
       return power;
     });
   };
   return powerValues(smallestDistances, get_reach_points(linkStation));
 }
 
-console.log(power(linkStation,total_distance(testValue,get_linkStation_points(linkStation))));
+function display_best_link_station(linkStation) {
+  linkStation.map((point) => {
+    console.log("point", point[0]);
+  });
+  var b = `Best link station for point x,y is x,y with power`;
+  return b;
+}
+
+// console.log(display_best_link_station(linkStation));
+
+console.log(
+  get_power(
+    linkStation,
+    total_distance(testValue, get_linkStation_points(linkStation))
+  )
+);
 
 // console.log(total_distance(testValue, get_linkStation_points(linkStation)));
