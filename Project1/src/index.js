6331a5a806ffe5a0f7ee41a84ba7b2aa2b7b2eff
Safiul Alam <82179767+safiulalam99@users.noreply.github.com>
2022-11-06 00:49:06 +0200

    Local setup

--- Project1/src/index.js
+++ Project1/src/index.js
@@ -1,27 +1,40 @@
 const express = require("express");
 
-var responseArrays = [];
-
-// function to calculate distance between point and link station (values sent as parameter in form of [x,y])
 function device_distance(testValue, linkStationPoints) {
-  var dist,
-    smallest = [99999];
+  /**
+    function to calculate the shortest distance between deviice and link station 
+
+   * @param    device[x][y], linkstation[x,y,r]  
+   * @return   device_distance object with linkstation(x,y), testValue(x,y) and power
+   */
+  var distance,
+    maxValue = [9999999];
   for (var i = 0; i < linkStationPoints.length; i++) {
-    dist = Math.sqrt(
+    //Distance formula
+    distance = Math.sqrt(
       Math.pow(testValue[0] - linkStationPoints[i][0], 2) +
         Math.pow(testValue[1] - linkStationPoints[i][1], 2)
     );
-    if (dist < smallest[0]) {
-      smallest[0] = dist;
-      smallest[1] = [linkStationPoints[i][0], linkStationPoints[i][1]];
-      smallest[2] = linkStationPoints[i][2];
-      smallest[3] = testValue;
+    // retrieving smallest distance for device (x,y) to the link station (x,y)
+    if (distance < maxValue[0]) {
+      maxValue[0] = distance;
+      maxValue[1] = [linkStationPoints[i][0], linkStationPoints[i][1]];
+      maxValue[2] = linkStationPoints[i][2];
+      maxValue[3] = testValue;
     }
   }
-  return smallest;
+  return maxValue;
 }
 
-function getPower(smallest) {
+function get_highest_power(smallest) {
+  /**
+   * get_highest_power calculates the power from the data returned by {device_distance} function
+     and compares to the output message
+
+   * @param   device_distance object with linkstation(x,y), testValue(x,y) and power
+   * @return   output message  
+   */
+
   if (smallest[0] > smallest[smallest.length - 2]) {
     return `No Link Station within reach for point (${smallest[3]})`;
   } else {
@@ -33,7 +46,15 @@ function getPower(smallest) {
   }
 }
 
-exports.main = (req, res) => {
+function test() {
+  /**
+   * Run the program for given sample data
+    
+   * @return   {Array}         Output message
+   */
+
+  var output = [];
+
   var linkStation = [
     [0, 0, 10],
     [20, 20, 5],
@@ -45,13 +66,26 @@ exports.main = (req, res) => {
     [100, 100],
     [15, 10],
     [18, 18],
-
   ];
-  if (responseArrays.length == 0) {
+
+  if (output.length == 0) {
     for (var x = 0; x < testValue.length; x++) {
-      responseArrays.push(getPower(device_distance(testValue[x], linkStation)));
+      output.push(
+        get_highest_power(device_distance(testValue[x], linkStation))
+      );
     }
-    res.status(200).send(responseArrays);
   }
-  responseArrays = [];
+  return output;
+}
+
+exports.main = (request, response) => {
+  /*
+    main executes and runs in cloud function
+   */
+  response.status(200).send(test());
 };
+
+/* executes when run locally */
+if (require.main === module) {
+  console.log(test());
+}
