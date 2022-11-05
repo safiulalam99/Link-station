81feefe387cfdafc393259c963a0181f7c62a8b3
Safiul Alam <82179767+safiulalam99@users.noreply.github.com>
2022-11-05 21:30:23 +0200

    Deploy to google cloud function with Terraform

---
+++ Project1/src/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+
+var responseArrays = [];
+
+// function to calculate distance between point and link station (values sent as parameter in form of [x,y])
+function device_distance(testValue, linkStationPoints) {
+  var dist,
+    smallest = [99999];
+  for (var i = 0; i < linkStationPoints.length; i++) {
+    dist = Math.sqrt(
+      Math.pow(testValue[0] - linkStationPoints[i][0], 2) +
+        Math.pow(testValue[1] - linkStationPoints[i][1], 2)
+    );
+    if (dist < smallest[0]) {
+      smallest[0] = dist;
+      smallest[1] = [linkStationPoints[i][0], linkStationPoints[i][1]];
+      smallest[2] = linkStationPoints[i][2];
+      smallest[3] = testValue;
+    }
+  }
+  return smallest;
+}
+
+function getPower(smallest) {
+  if (smallest[0] > smallest[smallest.length - 2]) {
+    return `No Link Station within reach for point (${smallest[3]})`;
+  } else {
+    var power = Math.pow(
+      smallest[smallest.length - 2] - smallest[0],
+      2
+    ).toFixed(2);
+    return `Best link station for (${smallest[3]}) is (${smallest[1]}) with power ${power}`;
+  }
+}
+
+exports.main = (req, res) => {
+  var linkStation = [
+    [0, 0, 10],
+    [20, 20, 5],
+    [10, 0, 12],
+  ];
+
+  var testValue = [
+    [0, 0],
+    [100, 100],
+    [15, 10],
+    [18, 18],
+
+  ];
+  if (responseArrays.length == 0) {
+    for (var x = 0; x < testValue.length; x++) {
+      responseArrays.push(getPower(device_distance(testValue[x], linkStation)));
+    }
+    res.status(200).send(responseArrays);
+  }
+  responseArrays = [];
+};
---
+++ Project1/terraform/main.tf
@@ -0,0 +1,51 @@
+locals {
+  project_id = "things-367718"
+  timestamp  = formatdate("YYMMDDhhmmss", timestamp())
+}
+
+provider "google" {
+  project     = local.project_id
+  region      = "us-central1"
+}
+
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = "../src"
+  output_path = "/tmp/git-function-${local.timestamp}.zip"
+}
+
+
+resource "google_storage_bucket" "bucket" {
+  name     = "${local.project_id}-functions"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "terraform-function.zip#${data.archive_file.source.output_md5}"
+  bucket = google_storage_bucket.bucket.name
+  source = data.archive_file.source.output_path
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name    = "terraform-function"
+  runtime = "nodejs16"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "main"
+}
+
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+output "function_url" {
+  value = google_cloudfunctions_function.function.https_trigger_url
+}
